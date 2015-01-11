2a30fada37d951ad129b495ebce10126d3725f28
Gabriel-LG <glamoursterbert@gmail.com>
2015-01-11 11:29:00 +0100

    Added AT demo v0.20 example

---
+++ examples/at_demo/at_demo/module.mk
@@ -0,0 +1,10 @@
+#Boilerplate to determine module path, name and build path
+MODULE_PATH       := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+MODULE_NAME       := $(notdir $(patsubst %/,%,$(MODULE_PATH)))
+MODULE_BUILD_PATH := $(BUILD_PATH)/$(MODULE_NAME)
+
+#  All paths must be absolute since they may be used for building from different paths.
+LIBS    += $(MODULE_NAME)
+LIBDIR  += $(MODULE_BUILD_PATH)/lib
+INCDIR  += $(MODULE_PATH)/include
+
